a0c65771b173b70dded6ce54278b62efef3d9229
Devin Wilson <craigvin84@yahoo.com>
2020-11-25 00:59:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,6 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+doc.css(".inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
 
 puts doc.css(".inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
