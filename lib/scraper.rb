c5152c1c3b7dccf34b13c99210cd12a60c0c153c
Devin Wilson <craigvin84@yahoo.com>
2020-11-25 01:12:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,3 +7,9 @@ doc = Nokogiri::HTML(html)
 doc.css(".title-oE5vT4")
 
 puts doc.css(".title-oE5vT4")
+
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+
+courses.each do |course|
+  puts course.text.strip
+end
