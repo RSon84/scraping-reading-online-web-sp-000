8888da71264b4a209a5e84a889ad986d920fcd1e
Devin Wilson <craigvin84@yahoo.com>
2020-11-25 01:18:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,9 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-doc.css(".title-oE5vT4")
 
-puts doc.css(".title-oE5vT4")
 
-courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
   puts course.text.strip
